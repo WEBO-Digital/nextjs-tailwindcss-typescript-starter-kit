9d50cf5db881669a107c1778a9aadf4909e35c84
Nirajan Basnet <er.nirajan.basnet@gmail.com>
2022-03-22 10:12:05 +0545

    Initial Setup

---
+++ components/module/footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent } from 'react';
+
+const Footer: FunctionComponent = () => {
+  return (
+    <footer className="p-5">
+      <div className="container mx-auto">
+        <div>This is a footer section</div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
